7a2596ff68cde42ed1f86962fc69055517bf99fc
Andrea Ambrosini <andrea.ambrosini@rossonet.com>
2025-10-21 12:50:01 +0200

    client plugin annotation

---
+++ waldot-api/src/main/java/net/rossonet/waldot/api/annotation/WaldotCliPlugin.java
@@ -0,0 +1,19 @@
+package net.rossonet.waldot.api.annotation;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+/**
+ * Annotation to mark a class as a Waldot Client Plugin. This annotation must be
+ * used to indicate that the class is a plugin with lifecycle methods
+ * 
+ * @Author Andrea Ambrosini - Rossonet s.c.a.r.l.
+ */
+@Retention(RUNTIME)
+@Target(TYPE)
+public @interface WaldotCliPlugin {
+
+}
